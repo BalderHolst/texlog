16faece5c24975cbf55bf8a1bc253453bdeb5245
BalderHolst <balderwh@gmail.com>
2023-10-15 12:58:12 +0200

    No more optional tokens

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,4 @@
-use std::{
-    fs,
-    path::{Path, PathBuf},
-};
+use std::{fs, path::PathBuf};
 
 use crate::lexer::{self, Token, TokenKind};
 
@@ -25,7 +22,8 @@ struct Node {
     messages: String,
 
     /// Position of the node in log file
-    pos: usize,
+    start_pos: usize,
+    end_pos: usize,
 
     /// The other files that this one calls
     calls: Vec<Node>,
@@ -35,16 +33,16 @@ impl Node {
     pub fn row_col(&self, source: &String) -> (usize, usize) {
         let mut row = 1;
         let mut last_newline = 0;
-        for (i, c) in source[..self.pos].chars().enumerate() {
+        for (i, c) in source[..self.start_pos].chars().enumerate() {
             match c {
                 '\n' => {
                     row += 1;
                     last_newline = i;
-                },
-                _ => {},
+                }
+                _ => {}
             }
         }
-        (row, self.pos - last_newline)
+        (row, self.start_pos - last_newline)
     }
 }
 
@@ -61,38 +59,52 @@ impl Parser {
 
     fn peak(&self, offset: isize) -> &Token {
         let index = self.cursor as isize + offset;
-        if index < 0 {
-            panic!("Peaked out of range...")
-        }
-        else {
-            self.tokens.get(index as usize).unwrap()
-        }
+        self.tokens
+            .get(index.clamp(0, self.tokens.len() as isize - 1) as usize)
+            .expect("Index should be clamped to a valid index.")
     }
 
     /// Get token under cursor
-    fn current(&self) -> Option<&Token> {
-        self.tokens.get(self.cursor)
+    fn current(&self) -> &Token {
+        self.tokens
+            .get(self.cursor.clamp(0, self.tokens.len() - 1))
+            .expect("Index should be clamped to a valid index.")
     }
 
     /// Get token under cursor and increment cursor
-    fn consume(&mut self) -> Option<&Token> {
-        let token = self.tokens.get(self.cursor);
+    fn consume(&mut self) -> &Token {
+        if self.tokens.is_empty() {
+            eprintln!("Warning: Called `consume` but token stream is empty.");
+            self.tokens.push(Token {
+                kind: TokenKind::Message("".to_string()),
+                pos: 0,
+            });
+            return self.tokens.last().unwrap();
+        }
+
+        let token = match self.tokens.get(self.cursor) {
+            Some(t) => t,
+            None => {
+                println!("Warning: Tried to consume at the end of token stream.");
+                self.cursor -= 1;
+                self.tokens
+                    .last()
+                    .expect("This is handled by the if statement before this match")
+            }
+        };
         println!("{:?}", token);
         self.cursor += 1;
         token
     }
 
     fn parse_node(&mut self) -> Node {
-        let pos = self.current().unwrap().pos;
+        let pos = self.current().pos;
 
-        assert!(self
-            .consume()
-            .expect("There should always be a token here to call this function.")
-            .has_kind(&TokenKind::LeftParen));
+        assert!(self.consume().has_kind(&TokenKind::LeftParen));
 
-        let file = match &self.current().unwrap().kind {
+        let file = match &self.current().kind {
             TokenKind::Path(p) => p.clone(),
-            _ => "no path...".to_string()
+            _ => "no path...".to_string(),
         };
 
         println!("Starting node {file}");
@@ -103,32 +115,30 @@ impl Parser {
         let mut unclosed_text_parens: usize = 0;
 
         loop {
-            match &self.current().unwrap().kind {
+            match &self.current().kind {
                 TokenKind::LeftParen => {
                     if let TokenKind::Path(_) = self.peak(1).kind {
                         calls.push(self.parse_node())
-                    }
-                    else {
+                    } else {
                         messages += "(";
                         unclosed_text_parens += 1;
                         self.consume();
                     }
-                },
+                }
                 TokenKind::RightParen => {
                     if unclosed_text_parens > 0 {
                         messages += ")";
                         unclosed_text_parens -= 1;
                         self.consume();
-                    }
-                    else {
-                        println!("Ending node {file}");
-                        self.consume();
+                    } else {
+                        let end_token = self.consume();
                         return Node {
                             file,
                             messages,
-                            pos,
+                            start_pos: pos,
+                            end_pos: end_token.pos,
                             calls,
-                        }
+                        };
                     }
                 }
                 TokenKind::Path(p) => {
@@ -147,28 +157,24 @@ impl Parser {
         let mut info = "".to_string();
         println!("info...");
         loop {
-            match self.current() {
-                Some(token) => match &token.kind {
-                    TokenKind::LeftParen => {
-                        if let TokenKind::Path(_) = self.peak(1).kind {
-                            break
-                        }
-                        else {
-                            info += "(";
-                            self.consume();
-                        }
-                    },
-                    TokenKind::RightParen => {
-                        info += ")";
+            match &self.current().kind {
+                TokenKind::LeftParen => {
+                    if let TokenKind::Path(_) = self.peak(1).kind {
+                        break;
+                    } else {
+                        info += "(";
                         self.consume();
-                    },
-                    TokenKind::Path(p) => panic!("Log should not start with `path`: {p}"),
-                    TokenKind::Message(m) => {
-                        info += m.as_str();
-                        self.consume();
-                    },
-                },
-                None => todo!("No root node"),
+                    }
+                }
+                TokenKind::RightParen => {
+                    info += ")";
+                    self.consume();
+                }
+                TokenKind::Path(p) => panic!("Log should not start with `path`: {p}"),
+                TokenKind::Message(m) => {
+                    info += m.as_str();
+                    self.consume();
+                }
             }
         }
         println!("Beyond info!");
@@ -181,7 +187,6 @@ struct Log {
     info: String,
     root_node: Node,
 }
-
 
 trait Visitor {
     fn visit_node(&mut self, node: &Node) {
@@ -211,7 +216,13 @@ impl Printer {
 
 impl Visitor for Printer {
     fn visit_node(&mut self, node: &Node) {
-        println!("{}{:?} at {:?} (len: {})", "  ".repeat(self.level), node.file, node.row_col(&self.text), node.messages.len());
+        println!(
+            "{}{:?} at {:?} (len: {})",
+            "  ".repeat(self.level),
+            node.file,
+            node.row_col(&self.text),
+            node.messages.len()
+        );
         self.level += 1;
         self.do_visit_node(node);
         self.level -= 1;
